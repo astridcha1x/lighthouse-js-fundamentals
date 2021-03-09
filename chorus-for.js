9bc3fb974ff80990f0dc21caa7da7354e5d3eb77
Astrid Aguilar <mixaguilar@yahoo.com>
2021-03-09 19:07:42 +0000

    a function that determines whether or not a number is odd

--- chorus-for.js
+++ chorus-for.js
@@ -1,5 +1,5 @@
 const chorus = "Let's dance!";
-for (let repeat = 0; repeat < 10; repeat++) {
+for (let repeat = 0; repeat < 10; repeat++) { // requires 3 statements, ; sep
   console.log(chorus);
 }
 console.log("Until the sun comes up!");
---
+++ odd.js
@@ -0,0 +1,6 @@
+const isOdd = function (num) {
+  return num % 2 != 0; // checks if num is NOT even
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
