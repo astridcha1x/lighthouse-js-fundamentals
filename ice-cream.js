9a2370c3a9c8555b030d998fe08731949f7d8c01
Astrid Aguilar <mixaguilar@yahoo.com>
2021-03-12 20:47:14 +0000

    to delete

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// add "root beer" flavor to array
+iceCreamFlavours.push("root beer");
+
+// get the first flavor from the array
+console.log(iceCreamFlavours[0]);
+
+// get the last flavour from the array
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+// total number of flavors in the array
+console.log(iceCreamFlavours.length);
+
